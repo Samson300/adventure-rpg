cc797667b4b855c469fd812ecb1581046eceaed9
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-17 09:42:50 -0400

    adds map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Player from './features/player/index'
+import Player from './features/player/index';
+import Map from './features/map';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Map />
         <Player />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 
-import store from './config/store'
+import store from './config/store';
+
+window.store = store;
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
