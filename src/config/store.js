096d450ec86471ef1b63f8c37bbf8bd23424a7b0
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-17 08:37:12 -0400

    initial commit

---
+++ src/config/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+import playerReducer from '../features/player/reducer'
+
+const rootReducer = combineReducers({
+    player: playerReducer,
+})
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS__EXTENTION__ && Window.__REDUX_DEVTOOLS__EXTENTION__()
+)
---
+++ src/features/player/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Player(props) {
+    return (
+        <div>Player</div>
+    )
+}
+
+export default Player;
