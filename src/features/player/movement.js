0f361bd0c7d6e624e34bf0a820341d7e9b0ee902
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-17 14:08:02 -0400

    adds observeImpassable function so you can't walk over trees rocks

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -3,8 +3,8 @@ import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from '../../config/constants';
 
 export default function handleMovement(player) {
 
-    function getNewPosition(direction) {
-        const oldPos = store.getState().player.position
+    function getNewPosition(oldPos, direction) {
+        
         switch(direction) {
             case 'WEST':
                 return [ oldPos[0]-SPRITE_SIZE, oldPos[1] ]
@@ -20,21 +20,32 @@ export default function handleMovement(player) {
     function observeBoundaries(oldPos, newPos) {
         return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH - SPRITE_SIZE) && 
         (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT - SPRITE_SIZE)
-        ? newPos : oldPos
     }
 
     function observeImpassable(oldPos, newPos) {
-        return 
+        const tiles = store.getState().map.tiles
+        const y = newPos[1] / SPRITE_SIZE
+        const x = newPos[0] / SPRITE_SIZE
+        const nextTile = tiles[y][x]
+        return nextTile < 5
     }
 
-    function dispatchMove(direction) {
-        const oldPos = store.getState().player.position
+    function dispatchMove(newPos) {
+        
         store.dispatch({
             type: 'MOVE_PLAYER',
             payload: {
-                position: observeBoundaries(oldPos ,getNewPosition(direction))
+                position: newPos
             }
         })
+    }
+
+    function attemptMove(direction) {
+        const oldPos = store.getState().player.position
+        const newPos = getNewPosition(oldPos, direction)
+
+        if(observeBoundaries(oldPos, newPos) && observeImpassable(oldPos, newPos))
+            dispatchMove(newPos)
     }
 
     function handleKeyDown(e) {
@@ -42,16 +53,16 @@ export default function handleMovement(player) {
 
         switch(e.keyCode) {
             case 37:
-                return dispatchMove('WEST')
+                return attemptMove('WEST')
 
             case 38:
-                return dispatchMove('NORTH')
+                return attemptMove('NORTH')
 
             case 39:
-                return dispatchMove('EAST')
+                return attemptMove('EAST')
 
             case 40:
-                return dispatchMove('SOUTH')
+                return attemptMove('SOUTH')
         
             default:
                 console.log(e.keyCode)
